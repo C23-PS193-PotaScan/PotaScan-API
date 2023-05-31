36cb810b27b989b196cff1424dc23dc6ccc31e95
Fakhri Yusuf Alfiansyah <fakhriyalfiansyah@gmail.com>
2023-05-31 18:43:45 +0700

    changed host

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
-const db = new Sequelize('potascan_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('potascan_db', 'root', 'asd', {
+    host: "35.232.95.198",
     dialect: "mysql"
 });
 
