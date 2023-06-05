8a93358d4e7ef093ab80035728834a76dba6302e
Muhamad Ghozi <ghozi.wijaya14@gmail.com>
2023-06-05 14:28:27 +0700

    delete getUser, verify token for article API

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUsers, Register, Login, Logout } from '../controllers/Users.js'
+import { Register, Login, Logout } from '../controllers/Users.js'
 import { verifyToken } from '../middleware/VerifyToken.js'
 import { refreshToken } from '../controllers/RefreshToken.js'
 import { getListArticle, getArticleById, getArticleByCategory, postArticle, updateArticle, deleteArticle } from '../controllers/article.js'
@@ -7,18 +7,17 @@ import { getListArticle, getArticleById, getArticleByCategory, postArticle, upda
 const router = express.Router()
 
 //API Login
-router.get('/users', verifyToken, getUsers)
 router.post('/register', Register)
 router.post('/login', Login)
 router.get('/token', refreshToken)
 router.delete('/logout', Logout)
 
 //API article
-router.get('/article', getListArticle)
-router.get('/article/:articleId', getArticleById)
-router.get('/article/category/:category', getArticleByCategory)
-router.post('/article', postArticle)
-router.put('/article', updateArticle)
-router.delete('/article/:articleId', deleteArticle)
+router.get('/article', verifyToken, getListArticle)
+router.get('/article/:articleId', verifyToken, getArticleById)
+router.get('/article/category/:category', verifyToken, getArticleByCategory)
+router.post('/article', verifyToken, postArticle)
+router.put('/article', verifyToken, updateArticle)
+router.delete('/article/:articleId', verifyToken, deleteArticle)
 
 export default router
